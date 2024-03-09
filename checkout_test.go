1b9a8a734a2f2e1e4c441adc6ed02a77de9b8d58
MattJohns91 <mattjohnson0626@gmail.com>
2024-03-09 16:56:27 +0000

    main:refactor assertions to match testify expect / actual layout

--- checkout_test.go
+++ checkout_test.go
@@ -12,12 +12,12 @@ func TestScanFunc(t *testing.T) {
 
 	t.Run("Should return a map with correct counts and handle lower case entries when called", func(t *testing.T) {
 
-		expected := map[string]int{"A": 1, "B":1}
+		expected := map[string]int{"A": 1, "B": 1}
 		c.Scan("A")
 		c.Scan("b")
 
 		assert.Equal(c.Basket, expected)
-		
+
 	})
 
 }
@@ -25,31 +25,33 @@ func TestScanFunc(t *testing.T) {
 func TestGetTotalCountFunc(t *testing.T) {
 	assert := assert.New(t)
 
-	c := Checkout {Basket: map[string]int { "A" : 6, "B": 3, "C": 0}}
+	c := Checkout{Basket: map[string]int{"A": 1, "B": 3, "C": 0}}
 
 	testP := Prices{
-	"A" : 50,
-	"B" : 120,
-	"C" : 3,
-	"D" : 12,
-}
+		"A": 50,
+		"B": 120,
+		"C": 3,
+		"D": 12,
+	}
 
-	
+	testSpecPrices := SpecialPrices{
+		"A": {2, 20},
+		"C": {3, 1},
+	}
 
-	t.Run("Should return a total price using standard pricing", func(t *testing.T) {
+	t.Run("Should return a total price using standard pricing, when special price threshold not reached", func(t *testing.T) {
 
-		totalPrice :=c.GetTotalPrice(testP)
+		totalPrice := c.GetTotalPrice(testP, testSpecPrices)
 
-		assert.Equal(totalPrice,660)
+		assert.Equal(410, totalPrice)
 	})
 
 	t.Run("Should return a total price using standard and discount pricing", func(t *testing.T) {
 
-		totalPrice :=c.GetTotalPrice(testP)
+		c := Checkout{Basket: map[string]int{"A": 8, "B": 4, "C": 6}}
+		totalPrice := c.GetTotalPrice(testP, testSpecPrices)
 
-		assert.Equal(totalPrice,500)
+		assert.Equal(562, totalPrice)
 	})
 
 }
-
-
